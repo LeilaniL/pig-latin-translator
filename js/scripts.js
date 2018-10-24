c10f01b7f3b55a17dde8d1ab33ee0e38fea17a98
Alex Williams and Leilani Leach <alex@>
2018-10-24 15:06:33 -0700

    Finished Pig Latin Translator. Using regular expressions, search function, switch and substr.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,48 @@
 // Business Logic here
-var piggy = function(index) {
-  if (/\d/g.test(index) === true) {
-    return index;
-  } else {
-    var index = ("That is not true");
-    return index;
-  }  
+var outputArray = [];
+var piggy = function(wordsArray) {
+  wordsArray.forEach(function(word) {
+    var char = word.search(/[aeiou]/g);
+    var charQU = word.search(/(qu)/g);
+    var charNUM = word.search(/\d/g);
+    //debugger;
+    if (charNUM === 0) {
+      switch(charNUM) {
+        default:
+          word = word;
+          break;
+      }
+    } else if (charQU === 0) {
+      switch(charQU) {
+        default:
+          word = word.replace("qu", "") + word.substr(charQU,2) + "ay";  
+          break;
+      }
+    } else {
+    switch(char) {
+      case 0:
+        word = word + "way";
+        break;
+      case - 1:
+        word = word + "ay";
+        break;
+      default:
+        word = word.substr(char) + word.substr(0,char) + "ay";
+    }
+    }
+    outputArray.push(word);
+  }); 
 };
-
-
 
 // User Interface Logic below
 $(document).ready(function() {
   $("form#formInput").submit(function(event) {
     event.preventDefault();
     var sentence = $("input#userInput").val();
-    $("#userOutput").text(piggy(sentence));
-    console.log(sentence);
+    var wordsArray = sentence.split(" ");
+    piggy(wordsArray);
+    newOutputArray = outputArray.join(" ");
+    $("#userOutput").text(newOutputArray);
 
   });
 });
